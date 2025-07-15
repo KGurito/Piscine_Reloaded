c5838fbc23c0e844f5c09232b122da873debc2eb
Gustavo Soares Silva <gusoares@c3r1p1.42sp.org.br>
2025-07-15 17:06:18 -0300

    ex14, ex15

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusoares <gusoares@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 16:55:06 by gusoares          #+#    #+#             */
+/*   Updated: 2025/07/15 17:05:49 by gusoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str);
+void	ft_putchar(char caracter);
+
+// void	ft_putchar(char caracter)
+// {
+// 	write(1, &caracter, 1);
+// }
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_putstr("gustavo");
+// }
